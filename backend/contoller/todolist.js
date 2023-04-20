c02f8310c322073ea3884e2f10e304c853350236
potato <jaafariramadan@gmail.com>
2023-04-20 20:19:35 +0300

    Declared Controller Methods - No implementation

---
+++ backend/contoller/todolist.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const TodoListContoller = class {
+    /** TODO
+     * 
+     * @param {String} title Task title
+     * @param {String} description Task description
+     *
+     * @returns the Id of the new task 
+    */
+    addTask(title, description) {
+
+    }
+    
+    /** TODO
+     * 
+     * @returns an array of Task objects
+     */
+    getTasks() {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id
+     * 
+     * @returns a Task Object 
+     */
+    getTask(id) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     * @param {String} title Task title
+     * @param {String} description Task description
+     * 
+     * @returns true if the task was updated successfully / false otherwise
+     */
+    updateTask(id, title, description) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     * 
+     * @returns true if the task was deleted successfully / false otherwise
+     */
+    deleteTask(id) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     */
+    checkTask(id) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     */
+    unCheckTask(id) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     */
+    pinTask(id) {
+
+    }
+
+    /** TODO
+     * 
+     * @param {Number} id Task Id
+     */
+    unPinTask(id) {
+        
+    }
+};
