16500a9d2593b5b542f718200db67df2561cd51f
Ramadan Jaafari <58656241+jaafari-R@users.noreply.github.com>
2023-05-06 17:01:09 +0000

    Task View CSS

--- frontend/src/components/TaskView.js
+++ frontend/src/components/TaskView.js
@@ -7,12 +7,14 @@ import './TaskView.css'
 function TaskView() {
   return (
     <div className='view-task'>
+      <div className="view-taskContent">
         <h2>Title</h2>
         <p>Description</p>
-        <button>Edit</button>
-        <PushPinIcon className='view-taskPin' />
-        <button>Check</button>
-        <button>Delete</button>
+      </div>
+      <PushPinIcon className='view-taskPin' />
+      <button className='view-taskCheck'>Done</button>
+      <button className='view-taskEdit'>Edit</button>
+      <button className='view-taskDelete'>Delete</button>
     </div>
   )
 }
