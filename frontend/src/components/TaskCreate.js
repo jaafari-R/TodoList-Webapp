061a34675943a83e8b4a650398eb55b30c5c7781
potato <jaafariramadan@gmail.com>
2023-06-12 23:52:18 +0300

    Changed Create task CSS

--- frontend/src/components/TaskCreate.js
+++ frontend/src/components/TaskCreate.js
@@ -6,37 +6,49 @@ import './TaskCreate.css'
 
 
 function TaskCreate({addTask, notify}) {
-    const [taskTitle, setTaskTitle] = useState("");
+	const [taskTitle, setTaskTitle] = useState("");
 
-    const handleChange = (setState) => (e) => {
-        setState(e.target.value);
-    }
+	const handleChange = (setState) => (e) => {
+		setState(e.target.value);
+	}
 
-    /** TODO create task locally & give it the id returned from the server
-     * Sends a create task request to the server
-     * 
-     * @returns created Task's Id on success
-     */ 
-    const createTask = async (e) => {
-        e.preventDefault();
-        todoListAPI.createTask(taskTitle)
-            .then((response) => {
-                notify({...response, msg: `Task ${response.newTask.title} was created successfully`});
-                addTask(response.newTask)
-            })
-            .catch((response) => {
-                notify(response)
-            });
-    }
+	/** TODO create task locally & give it the id returned from the server
+	 * Sends a create task request to the server
+	 * 
+	 * @returns created Task's Id on success
+	 */ 
+	const createTask = async (e) => {
+		e.preventDefault();
+		todoListAPI.createTask(taskTitle)
+			.then((response) => {
+				notify({
+					...response,
+					msg: `Task ${response.newTask.title} was created successfully`
+				});
+				addTask(response.newTask)
+				setTaskTitle('');
+			})
+			.catch((response) => {
+				notify(response)
+			});
+	}
 
   return (
     <div>
-        <form className="create-task" action="">
-            <h1>Create a Task</h1>
-            <input className="create-taskTitle" id="taskTitle" type="text" value={taskTitle} onChange={handleChange(setTaskTitle)} placeholder='Task Title'/>
-            <br />
-            <button className="create-taskButton" onClick={createTask}>Create Task</button>
-        </form>
+			<form className="create-task" action="">
+				<h1>Create a Task</h1>
+				<input 
+					value={taskTitle} 
+					onChange={handleChange(setTaskTitle)} 
+					type="text" 
+					placeholder='Task Title'
+				/>
+				<button 
+					onClick={createTask}  
+					type='submit'>
+						Create Task
+				</button>
+			</form>
     </div>
   )
 }
